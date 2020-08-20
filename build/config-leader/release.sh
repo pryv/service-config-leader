721d479b11fda03401c5ac85d21133d5ae9ebe56
Michal Gil <michalgil@pryvmbp.home>
2020-08-20 16:17:42 +0200

    Deployment workflow

--- build/config-leader/release.sh
+++ build/config-leader/release.sh
@@ -16,8 +16,7 @@ minimal_apt_get_install unicode-data
 
 # Unpack the application and run npm install. 
 pushd $target_dir
-run run tar -x --owner app -f \
-  /pd_build/release.tar .
+run run tar --owner app -xf /pd_build/release.tar
 
 PYTHON=$(which python2.7) run yarn install
 
